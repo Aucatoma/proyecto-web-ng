7cc384ef16892182da5d45e0d6eb2d0dfd9333e7
Daniel Aucatoma <danielaucatoma1996@hotmail.com>
2018-07-26 17:35:46 -0500

    Se añade la librería semantic.css

--- src/app/carrusel/carrusel.component.ts
+++ src/app/carrusel/carrusel.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+declare var $: any;
+
 
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
   styleUrls: ['./carrusel.component.css']
 })
-export class CarruselComponent implements OnInit {
+export class CarruselComponent implements OnInit, AfterViewInit {
+  content;
 
-  constructor() { }
+  ngAfterViewInit(): void {
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
 
 }
