8a078e85c5330eaf4537dda33fb61d5763a67042
magallegos1996 <bwcekos@gmail.com>
2018-08-02 14:17:55 -0500

    Se controla de mejor manera si el usuario inició sesión o no y se modifica la clase Comentario para que acepte

--- src/app/entidades/comentario.ts
+++ src/app/entidades/comentario.ts
@@ -5,12 +5,16 @@ export class Comentario {
   puntuacionLibro: number;
   libro: number;
   usuario: number;
+  username: string;
+  avatarUsuario: string;
   constructor() {
     this.fecha = '';
     this.comentario = '';
     this.puntuacionLibro = 0;
     this.libro = 0;
     this.usuario = 0;
+    this.username = '';
+    this.avatarUsuario = '';
   }
 }
 
