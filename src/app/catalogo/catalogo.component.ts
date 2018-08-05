3deb46b8a0433124c0ed091477c2b705b3e3da97
magallegos1996 <bwcekos@gmail.com>
2018-08-04 20:28:07 -0500

    Interfaz de Pedido lista con tarjetas

--- src/app/catalogo/catalogo.component.ts
+++ src/app/catalogo/catalogo.component.ts
@@ -13,7 +13,6 @@ import {Comentario} from '../entidades/comentario';
 export class CatalogoComponent implements OnInit {
 
   libros: Libro[];
-  puntuacion = 3;
 
   constructor(
     private _router: Router,
@@ -26,7 +25,6 @@ export class CatalogoComponent implements OnInit {
     const libros$ = this._libroService.obtenerTodos();
     libros$.subscribe(value => {
       this.libros = value;
-      this.obtenerPuntajeLibros();
     }, error1 => console.log(error1));
   }
 
@@ -39,37 +37,6 @@ export class CatalogoComponent implements OnInit {
       libros$.subscribe(value => {
         this.libros = value;
       }, error1 => console.log(error1));
-    }
-  }
-
-  obtenerPuntajeLibros() {
-    for (let _i = 0; _i < this.libros.length; _i++) {
-      this.obtenerComentarios(this.libros[_i].id + '', this.libros[_i]);
-    }
-  }
-
-  obtenerComentarios(id: string, libro: Libro) {
-    const comentarios$ = this._comentarioService.obtenerPorLibroId(id);
-    comentarios$.subscribe((value) => {
-      libro.puntuacion = this.obtenerPuntaje(value);
-    }, (error1) => {
-      console.log(error1);
-    });
-    return this.puntuacion;
-  }
-
-  obtenerPuntaje(comentarios: Comentario[]): number {
-    if (comentarios.length !== 0) {
-      let total = 0;
-      for (const comentario of comentarios) {
-        total = comentario.puntuacionLibro + total;
-      }
-      console.log('MIS PUNTJAES: ' + Math.round(total / comentarios.length));
-      this.puntuacion = Math.round(total / comentarios.length);
-      return Math.round(total / comentarios.length);
-    } else {
-      this.puntuacion = 0;
-      return 0;
     }
   }
 }
